0fb2e409833d7a7a3d974d5277e1bc516efa49fb
Rizwan Khot <khotrizwan@gmail.com>
2021-08-29 13:37:46 +0530

    Spring Boot Hello World Demo

---
+++ src/test/java/com/rizwan/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizwan.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
